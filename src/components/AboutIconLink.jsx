17652fb058b6d0f1988c8d963dfe2327995bbf47
Lin Liu <liulinn60@gmail.com>
2022-02-18 16:59:57 -0800

    pretty much everything is done

---
+++ src/components/AboutIconLink.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {FaQuestion} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+
+function AboutIconLink() {
+  return (
+    <div className='about-link '>
+        {/* <Link to='/about'> */}
+        
+        {/* ways to pass different properties */}
+        <Link to={{
+            pathname: '/about',
+            search: '?sort=name',
+            hash: '#hello',
+        }}>
+            <FaQuestion size={30} />
+        </Link>
+        
+    </div>
+  )
+}
+
+export default AboutIconLink 
